a17757edb68e7baf006ad59074cde3433a35acda
Derrick Bundi <derrick.bundi27@gmail.com>
2022-10-24 10:29:32 +0300

    new

--- app/Http/Controllers/smscontroller.php
+++ app/Http/Controllers/smscontroller.php
@@ -8,16 +8,23 @@ use AfricasTalking\SDK\AfricasTalking;
 class smscontroller extends Controller
 {
     public function send_sms($contact, $message) {
-        $apiKey = config('services.africastalking_key');
-        $username = config('services.africastalking_secret');
-        $env = config('services.africastalking_env');
-        if($env == "prod") {
-            $AT = new AfricasTalking($username, $apiKey);
-            $sms = $AT->sms();
-            $result = $sms->send([
-                'to'      => $contact,
-                'message' => $message
-            ]);
-        }
+        // $apiKey = config('services.africastalking_key');
+        // $username = config('services.africastalking_secret');
+        // $env = config('services.africastalking_env');
+        // if($env == "prod") {
+        //     $AT = new AfricasTalking($username, $apiKey);
+        //     $sms = $AT->sms();
+        //     $result = $sms->send([
+        //         'to'      => $contact,
+        //         'message' => $message
+        //     ]);
+        // }
+        $at = new AfricasTalking(config('services.africastalking_secret'), config('services.africastalking_key'));
+        $sms = $at->sms();
+        $result = $sms->send([
+            'to' => 0 . substr($contact,-9),
+            'message' => $message
+        ]);
+        Log::critical("Message sent\r\n".$message."to\r\n".$contact);
     }
 }
