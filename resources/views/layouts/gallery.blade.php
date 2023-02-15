5f9a6ebce7f3d82ac2937ccb0b8b8acbedec6f37
nzai <nzaih18@gmail.com>
2023-02-15 20:17:24 +0300

    lol

--- resources/views/layouts/gallery.blade.php
+++ resources/views/layouts/gallery.blade.php
@@ -1,32 +1,34 @@
 <style>
-    .lightbox-gallery{background-image: linear-gradient(#4A148C, #E53935);background-repeat: no-repeat;color: #000;overflow-x: hidden}.lightbox-gallery p{color:#fff}.lightbox-gallery h2{font-weight:bold;margin-bottom:40px;padding-top:40px;color:#fff}@media (max-width:767px){.lightbox-gallery h2{margin-bottom:25px;padding-top:25px;font-size:24px}}.lightbox-gallery .intro{font-size:16px;max-width:500px;margin:0 auto 40px}.lightbox-gallery .intro p{margin-bottom:0}.lightbox-gallery .photos{padding-bottom:20px}.lightbox-gallery .item{padding-bottom:30px}
+    .lightbox-gallery{overflow-x: hidden}.lightbox-gallery p{color:#fff}.lightbox-gallery h2{font-weight:bold;margin-bottom:40px;padding-top:40px;color:#fff}@media (max-width:767px){.lightbox-gallery h2{margin-bottom:25px;padding-top:25px;font-size:24px}}.lightbox-gallery .intro{font-size:16px;max-width:500px;margin:0 auto 40px}.lightbox-gallery .intro p{margin-bottom:0}.lightbox-gallery .photos{padding-bottom:20px}.lightbox-gallery .item{padding-bottom:30px}
 </style>
 
 
 
 
-<div class="lightbox-gallery">
+<div class="lightbox-gallery tw-bg-gradient-to-r tw-from-rose-100 tw-to-teal-100">
     <div class="container">
         <div class="intro">
-            <h2 class="text-center">Lightbox Gallery</h2>
-            <p class="text-center">Find the lightbox gallery for your project. click on any image to open gallary</p>
+            
+           <h1 class=" w-font-bold tw-p-4 tw-text-4xl tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-r tw-from-amber-500 tw-to-pink-500 tw-font-sans">Check Out Our Gallery</h1>
         </div>
         <div class="row photos">
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} " data-lightbox="photos"><img class="img-fluid" src= "{{ asset('theme_one/images/logo.png') }} "></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} "data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} ">
-          </a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} " data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} "></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} " data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} "></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} " data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} "></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} " data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} "></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} " data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} "></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} " data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} "></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} " data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} "></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} " data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} "></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} " data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} "></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} " data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} "></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} " data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} "></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('theme_one/images/logo.png') }} "data-lightbox="photos"><img class="img-fluid" src="{{ asset('theme_one/images/logo.png') }} "></a></div>
+            <div class="col-sm-6 col-md-4 col-lg-3 item "><a href="{{ asset('theme_one/images/artist1.jpeg') }} " data-lightbox="photos" class=""><img class="img-fluid tw-rounded tw-p-0 tw-h-72 tw-w-full tw-block tw-m-auto tw-text-center" src= "{{ asset('theme_one/images/artist1.jpeg') }} "></a></div>
+
+             <div class="col-sm-6 col-md-4 col-lg-3 item "><a href="{{ asset('theme_one/images/artist2.jpeg') }} " data-lightbox="photos"><img class="img-fluid tw-rounded tw-h-72 tw-w-full tw-block tw-m-auto tw-text-center tw-object-contsin" src= "{{ asset('theme_one/images/artist2.jpeg') }} "></a></div>
+
+              <div class="col-sm-6 col-md-4 col-lg-3 item "><a href="{{ asset('theme_one/images/artist3.jpeg') }} " data-lightbox="photos"><img class="img-fluid tw-rounded tw-h-72 tw-w-full tw-block tw-m-auto tw-text-center" src= "{{ asset('theme_one/images/artist3.jpeg') }} "></a></div>
+
+               <div class="col-sm-6 col-md-4 col-lg-3 item "><a href="{{ asset('theme_one/images/artist4.jpeg') }} " data-lightbox="photos"><img class="img-fluid tw-rounded tw-h-72 tw-w-full tw-block tw-m-auto tw-text-center" src= "{{ asset('theme_one/images/artist4.jpeg') }} "></a></div>
+
+            
+         
+
+            
+
+            
+            
+            
+            
           
         </div>
     </div>
